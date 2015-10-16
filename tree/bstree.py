c35715813b08d2443742da1e2dc221da20e5d0a9
djole <djordje_7@hotmail.com>
2015-10-16 14:17:28 -0400

    bst class

---
+++ tree/bstree.py
@@ -0,0 +1,21 @@
+class BST(self):
+	def __init__(self,root):
+		self.root = root
+
+	def root():
+		return root
+
+	def insert(self,new):
+		if self.root:
+			if new.value() <= self.root.value() : __insert(self,root.left,new)
+			else                                 : __insert(self,root.right,new)
+		else: self.root = node
+
+	def __insert(self,curr,new):
+		if curr is None:
+			curr = new
+		elif new.value() <= curr.value() : __insert(self,curr.left,new)
+		else                             : __insert(self,curr.right,new)
+
+class Node(self):
+	
