960d392fc5141a46d79b2ef033695640d70c640b
djole <djordje_7@hotmail.com>
2015-10-18 13:17:14 -0400

    pig game completed

--- pig.py
+++ pig.py
@@ -1,32 +1,55 @@
+from random import randint
+
 class Pig:
 
 	def __init__(self):
+		self.turn = 1
 		self.p1score = 0
 		self.p2score = 0
-		self.options = ""
+		self.options = "1 : Roll the dice!\n2: Hold your score"
 		self.onGoing = True
 		self.winner = None
+		print("WELCOME TO PIG!")
+
 	def move(self):
-		self.options()
-		move = int(input())
-		if move == 1:
-			rolled = roll()
-			#either gain nothing and return or re-move()
-			conditions(rolled)
-		elif move == 2:
-			return hold()
+		gains = 0
+		while(1):
+			print(self.options)
+			print("Your current gainzzzzz: %i" % gains)
+			move = int(input())
+			if move == 1:
+				rolled = self.roll()
+				print("You rolled a %i!!!!!" % rolled)
+				if rolled == 1: return 0
+				else:  gains+=rolled
+			elif move == 2:
+				return gains
+			else: print("Not a valid move punk!!")
 	
+	def roll(self):
+		return randint(1,6)
+
 	def checkWin(self):
-		pass
+		if self.p1score >= 100 or self.p2score >= 100:
+			self.onGoing = False
 
 	def startGame(self):
-		while(onGoing):
-			if turn = 0:
-				self.p1score = move()
-				checkWin()
+		while(self.onGoing):
+			self.turn ^=1
+			print("The score is\nPlayer 1: %i\nPlayer 2: %i" % (self.p1score,self.p2score))
+			if self.turn == 0:
+				print("Player 1's turn! Make a move buster")
+				self.p1score += self.move()
 			else:
-				self.p2score = move()
-		print("The winner is ___!!!")
+				print("Player 2's turn! Go for gold")
+				self.p2score += self.move()
+			self.checkWin()
+		print("The winner is !!!")
 
-	def options(self):
-		print(self.options)
+def main():
+	while(1):
+		newGame = Pig()
+		newGame.startGame()
+
+if __name__ == "__main__":
+	main()
