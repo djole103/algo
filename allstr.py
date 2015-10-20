f860942c6c3bc991abf3e6bf9d37097066cabe37
djole <djordje_7@hotmail.com>
2015-10-19 22:20:04 -0400

    all words from chars

--- allstr.py
+++ allstr.py
@@ -1,7 +1,22 @@
 rslt = []
 check = {}
 
-def allstr(chars):
-	if 
-	for i in range(chars):
-		allstr(chars[:i] + chars[i:-1])
+def realWord(string):
+	return True
+
+def allstr(string,chars): 
+	for i in range(len(chars)):
+		string+=chars[i]
+		if realWord(string):
+			rslt.append(string)
+		allstr(string,chars[:i]+chars[i+1:])
+		string = string[0:-1]
+		
+		
+
+chars = ['a','b']
+allstr("",chars)
+print(rslt)
+
+a = "abcde"
+print(a[:2]+a[3:])
