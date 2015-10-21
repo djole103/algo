4e164d376916939002f28e9b9c3fe7938ae15681
djole <djordje_7@hotmail.com>
2015-10-21 14:06:29 -0400

    depth and bfs traversal of graph find min duplicate prob

--- graph.py
+++ graph.py
@@ -10,16 +10,37 @@ def insert_duplicate(id1,id2):
 	else:
 		graph[id2] = [id1]
 
-def return_min(bus_id,stack=[],minVal=float('inf')):
+def return_minDEPTH(bus_id,visited={},minVal=float('inf')):
 	if bus_id < minVal: minVal = bus_id
-	stack.append(bus_id)
+	visited[bus_id] = True
 	for bus in graph[bus_id]:
-		if bus in stack: continue
-		else: minVal = return_min(bus,stack,minVal)
+		if bus in visited: continue
+		else: minVal = return_minDEPTH(bus,visited,minVal)
+	return minVal
+
+def return_minBFS(bus_id,q=[],minVal=float('inf'),queued={}):
+	queued[bus_id] = True
+	if bus_id < minVal: minVal = bus_id
+	for bus in graph[bus_id]:
+		if bus in queued: continue
+		q.append(bus)
+		queued[bus] = True
+	while q:
+		bus = q.pop(0)
+		minVal = return_minBFS(bus,q,minVal,queued)
 	return minVal
 
 insert_duplicate(4,2)
 insert_duplicate(4,1)
+insert_duplicate(1,0)
+insert_duplicate(0,5)
+insert_duplicate(0,6)
+insert_duplicate(0,7)
+insert_duplicate(6,5)
+insert_duplicate(5,11)
+insert_duplicate(6,12)
+insert_duplicate(2,11)
+insert_duplicate(2,12)
 for key,value in graph.items():
 	print(key,value)
-print(return_min(2))
+print(return_minDEPTH(2))
