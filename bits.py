8fac57ca7ed83a69c962021b554d08bb7eb91347
djole <djordje_7@hotmail.com>
2015-10-21 17:13:11 -0400

    int manip probs, stream track, global var stuff

---
+++ bits.py
@@ -0,0 +1,21 @@
+import math
+
+def count_ones(binary):
+	ones = 0
+	while(binary):
+		ones+=1
+		binary= binary & (binary-1)
+	return ones
+
+def palindrome(num):
+#	digits = math.ceil(math.log(num,10))
+#	print("Digits: ", digits)
+#	if digits == 1: return True
+	reverse = ""
+	original = num
+	while(num):
+		reverse += str(num%10)
+		num = math.floor(num/10)
+	if int(reverse) == original: return True
+	else: return False
+
---
+++ global.py
@@ -0,0 +1,7 @@
+glob = [None,None,None]
+
+def test():
+	global glob
+	print(glob)
+
+test()
