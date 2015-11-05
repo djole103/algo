51f49b4b2dade19cbdcabbff62443edccfc6d1dd
djole <djordje_7@hotmail.com>
2015-11-05 01:39:55 -0500

    factory pattern, incompatible type error when assigning

--- c/strct.c
+++ c/strct.c
@@ -4,12 +4,29 @@
 struct Dog {
 	char name[20];
 	char breed[20];
+	int id;
 };
+
+struct Dog dog_factory(char[20] name, char[20] breed,int id){
+	struct Dog newDog;
+	strcpy(newDog.name,name);
+	strcpy(newDog.breed,breed);
+	newDog.id = id;
+	return newDog;
+} 
+
+/*void dog_kill(struct* Dog){
+free memory	
+}*/
 
 int main(){
 	struct Dog rex;
 	strcpy( rex.name, "Rex");
 	strcpy( rex.breed, "Pitbull");
-	printf("%s", rex.name);
+	printf("%s\n", rex.name);
+	
+	struct Dog zeus;
+	zeus = dog_factory("Zeus", "Shiba Inu",1);
+	printf("%s\n",zeus.name);
 	return 1;
 }
