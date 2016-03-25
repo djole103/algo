e72ed0eab602da405820c554d909f6303b1f7af8
djole <djordje_7@hotmail.com>
2016-03-24 22:14:51 -0400

    improved bit counter

--- python/round2/countbits.py
+++ python/round2/countbits.py
@@ -2,9 +2,14 @@
 
 NUM = 5
 
-for i in range(1,NUM+1):
-  count = 0
-  while i:
-    i = i & (i-1)
-    count+=1
-  print(count)
+results = [0 for _ in range(NUM+1)]
+for i in range(NUM+1):
+  results[i] = results[i >> 1] + (i & 1)
+print(results)
+
+#for i in range(1,NUM+1):
+#  count = 0
+#  while i:
+#    i = i & (i-1)
+#    count+=1
+#  print(count)
