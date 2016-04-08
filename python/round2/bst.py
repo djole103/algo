6801774e3272e220d9a5107301002e3053c7fb50
djole <djordje_7@hotmail.com>
2016-04-07 20:45:11 -0400

    bst

---
+++ python/round2/bst.py
@@ -0,0 +1,43 @@
+ass Node(object):
+
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+
+def insert(root, data):
+    if root == None:
+        root = Node(data)
+        return root
+    if data <= root.data:
+        root.left = insert(root.left, data)
+        return root
+    else:
+        root.right = insert(root.right, data)
+        return root
+    
+def in_order_traversal(root, results):
+    if root == None:
+        return
+    stack = [root]
+    while stack:
+        node = stack.pop(0)
+        results.append(node.data)
+        if node.left!= None:
+            stack.append(node.left)
+        if node.right!= None:
+            stack.append(node.right)
+    
+def PrintNode(node):
+    print(node.data)
+    print(node.left.data)
+    print(node.right.data)
+results = []
+root = Node(5)
+insert(root, 2)
+insert(root, 8)
+insert(root, 1)
+insert(root, 3)
+in_order_traversal(root, results)
+print(results)
