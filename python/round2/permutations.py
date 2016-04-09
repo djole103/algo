315513539dd670aac47468c4d6d19f4c0f253a8e
djole <djordje_7@hotmail.com>
2016-04-08 22:38:25 -0400

    permutations, using too much storage think of a non-recursive no additional storage way

---
+++ python/round2/permutations.py
@@ -0,0 +1,15 @@
+import collections 
+
+#O(n) storage
+def permutations(str1, str2):
+    if str1 == None or str2 == None: return False
+    d = collections.defaultdict(int)
+    for l in str1:
+        d[l] +=1
+    for l in str2:
+        if l not in d:
+            return False
+        d[l] -= 1
+        if d[l] < 0:
+            return False
+    return True
