ca09f0430b86f23593504a37cbec5a86ee95d548
djole <djordje_7@hotmail.com>
2016-04-16 00:22:10 -0400

    beautiful

--- python/round2/fibonacci.py
+++ python/round2/fibonacci.py
@@ -23,7 +23,17 @@ def fib3(n):
     cache.append(cache[i] + cache[i-1])
   return cache[n-1]
 
+def fib4(n):
+  b1 = 1
+  b2 = 0
+  if n == 1: return b2
+  if n == 2: return b1
+
+  for i in range(3,n):
+    b2, b1 = b1, b1+b2
+  return b1+b2
+
 print(fib(8))
 print(fib2(8))
 print(fib3(8))
-
+print(fib4(8))
