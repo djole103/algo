e7a1730bef1a18c437820f0f3e8f09ba74fdd553
djole <djordje_7@hotmail.com>
2016-09-30 10:29:29 -0400

    psuedo

--- cplus/app.cc
+++ cplus/app.cc
@@ -4,10 +4,25 @@
 using std::vector;
 using std::string;
 
-string* all_permutations(string alphabet[]){
-  std::cout << "wip"; 
-  static string permutations[] = {"ok", "hey"};
-  return permutations;
+/*
+ *can use static string array
+ *or can pass pointer
+*/
+
+
+//doing it with pointers and updating the actual memory
+string* all_combinations(string alphabet[], string word = "", string[] results = []){
+  if(alphabet.length() == 0){
+    return [];
+  }
+
+  for(int i=0;i<alphabet.length();i++){
+    string result = word + alphabet[i];
+    results.append(result);
+    all_combinations(alphabet[:i] + alphabet[i+1:], result, results);
+  }
+
+  return results;
 }
 
 int main(){
